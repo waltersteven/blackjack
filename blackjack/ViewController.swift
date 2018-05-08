c39c5790e9bb5988df254b652686a017a83c3083
ITLAB <itlab@ulima.edu.pe>
2018-05-08 14:38:56 -0500

    Initial Commit

---
+++ blackjack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  blackjack
+//
+//  Created by ulima on 8/05/18.
+//  Copyright © 2018 ulima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
